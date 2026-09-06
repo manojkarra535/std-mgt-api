f32d2d08b4a1c2494f8d16676acb6efe39a0922d
manoj <karramanoj535@gmail.com>
2026-09-06 14:03:44 +0530

    nodejs server api created

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+// Home API
+app.get("/", (req, res) => {
+  res.send("Node.js API is running!");
+});
+
+// Get users
+app.get("/api/users", (req, res) => {
+  res.json([
+    { id: 1, name: "Manoj" },
+    { id: 2, name: "John" }
+  ]);
+});
+
+// Create user
+app.post("/api/users", (req, res) => {
+  const user = req.body;
+
+  res.status(201).json({
+    message: "User created successfully",
+    user: user
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
